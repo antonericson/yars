c59e54f56c06767e6d4135fc09fd807c331949a3
antonericson <anton@aericson.com>
2023-04-08 15:42:52 +0200

    refactored python code

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import tts
 import subprocess
 import json
+from TTS.api import TTS
+
+def getSentencesFromStory(full_story):
+    sentences = []
+    for sentence in full_story.split(". "):
+        for word in sentence.split(", "):
+            if len(word.split(" ")) > 20:
+                letters = word.split(" ")
+                sentences.append(' '.join(letters[:len(letters)//2]))
+                sentences.append(' '.join(letters[len(letters)//2:]))
+            else:
+                sentences.append(word)
+    return sentences
 
 def create_react_config(backgroundVideoName, sentences, videoLengths):
   config = {
@@ -16,13 +29,18 @@ def create_react_config(backgroundVideoName, sentences, videoLengths):
 def main():
 
     #all_text = "So my parents bought a house almost 3 years ago for me and my wife and kids to rent from them. They originally told us it would take a few months to a year to get ready so we agreed, they bought the house then asked us if we wanted to rent. We'll its been almost 3 years now and they still refuse to let my wifes side of the family help and now, me and my wife have the option to buy a home and we decided that that's what we wanted instead of renting. When we informed then of our decision they were not happy about it. It's been a month and they have still not responded to any of our messages or calls. We've tried reaching out to them and inviting them to our kids games and graduation but they have yet to even read the messages but we see them messaging in the family group chat about other family members events. So am I the asshole for deciding to buy a house instead of renting the one they bought?"
-    all_text = "So my parents bought a house almost 3 years ago for me and my wife and kids to rent from them."
+    test_story = "So my parents bought a house almost 3 years ago for me and my wife and kids to rent from them."
+    all_stories = [test_story]
 
-    create_react_config("gameplay_video.mp4", test_text_list, video_lengths)
+    tts_instance = TTS(model_name="tts_models/en/vctk/vits")
+    for story in all_stories:
+        sentences = getSentencesFromStory(story)
+        video_lengths = tts.generateTTSForSentences(tts_instance, sentences)
+
+    create_react_config("gameplay_video.mp4", sentences, video_lengths)
 
     generate_video_command = 'cd video-generation; npx remotion render RedditStory ../out/video.mp4 --props=../current-config.json'
     subprocess.run(generate_video_command, shell=True)
-    print("Hello World!")
 
 if __name__ == "__main__":
     main()
--- tts.py
+++ tts.py
@@ -1,23 +1,10 @@
 import contextlib
 import wave
-from TTS.api import TTS
 
 
-def generateTTSForStory(tts, full_story):
-  test_text_list = []
-
-  for sentence in full_story.split(". "):
-    for word in sentence.split(", "):
-      if len(word.split(" ")) > 20:
-        letters = word.split(" ")
-        test_text_list.append(' '.join(letters[:len(letters)//2]))
-        test_text_list.append(' '.join(letters[len(letters)//2:]))
-      else:
-        test_text_list.append(word)
-
+def generateTTSForSentences(tts, sentences):
   video_lengths = []
-  tts = TTS(model_name="tts_models/en/vctk/vits")
-  for i, sentence in enumerate(test_text_list):
+  for i, sentence in enumerate(sentences):
     path_for_react = f'audio/{i}.wav'
     full_path = f'video-generation/public/{path_for_react}'
     tts.tts_to_file(text=sentence, speaker="p273", file_path=full_path)
@@ -26,3 +13,5 @@ def generateTTSForStory(tts, full_story):
       rate = f.getframerate()
       duration = frames / float(rate)
       video_lengths.append(duration)
+  
+  return video_lengths
