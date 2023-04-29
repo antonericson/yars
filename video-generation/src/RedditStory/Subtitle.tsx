358eee9da89ac36a25dc45688f9818508a19ce5c
antonericson <anton@aericson.com>
2023-04-29 14:27:50 +0200

    Updated UI and generated posts

--- video-generation/src/RedditStory/Subtitle.tsx
+++ video-generation/src/RedditStory/Subtitle.tsx
@@ -7,10 +7,10 @@ import {
 } from 'remotion';
 
 export const Subtitle: React.FC<{
-	titleText: string;
-	audioFile: string;
-	numberOfFrames: number;
-}> = ({titleText, audioFile, numberOfFrames}) => {
+    titleText: string;
+    audioFile: string;
+    numberOfFrames: number;
+}> = ({ titleText, audioFile, numberOfFrames }) => {
     const videoConfig = useVideoConfig();
     const frame = useCurrentFrame();
     const text = titleText.split(' ').map((t) => ` ${t} `);
@@ -20,56 +20,71 @@ export const Subtitle: React.FC<{
     return (
         <>
             <Audio src={staticFile(audioFile)} />
-            <h1
-                style={{
-                    fontFamily: 'SF Pro Text, Helvetica, Arial',
-                    fontWeight: 'bold',
-                    fontSize: 70,
-                    textAlign: 'center',
-                    position: 'absolute',
-                    bottom: '50%',
-                    width: '100%',
-                    zIndex: 99,
-                    WebkitTextStroke: '2px black'
-                }}
-            >
-				
-                {text.map((t, i) => {
-					
-                    if(i === 0){
-                        usedFrames = 0
-                    }else{
-                        nFramesToUse = Math.round((numberOfFrames*0.80-usedFrames)/(nWords-i+1))
-                        usedFrames = usedFrames + nFramesToUse
-                    }
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css" />
+            <div style={{
+                height: '100%',
+                width: '100%',
+                marginTop: '60%',
+                alignContent: 'center',
+            }}>
+                <div style={{
+                    margin: '0 1em 0 1em',
+                    paddingBottom: '1em',
+                    paddingTop: '.5em',
+                    background: 'rgba(0, 0, 0, 0.3)',
+                    borderRadius: '1em'
+                }}>
+                    <h1
+                        style={{
+                            textAlign: 'center',
+                            fontFamily: 'Open Sans',
+                            fontStyle: 'bold',
+                            fontWeight: 600,
+                            fontSize: '5.3em',
+                            lineHeight: '98px',
+                            color: '#FFFFFF',
+                            WebkitTextStroke: '4px #000000'
+                        }}
+                    >
 
-                    return (
-                        <span
-                            key={t+'-'+i}
-                            style={{
-                                color: 'white',
-                                marginLeft: 20,
-                                marginRight: 20,
-                                transform: `scale(${spring({
-                                    fps: videoConfig.fps,
-                                    frame:  frame - usedFrames,
-                                    config: {
-                                        damping: 100,
-                                        stiffness: 200,
-                                        mass: 0.5,
-                                    },
-                                })})`,
-                                display: 'inline-block',
-                            }}
-                        >
-                            {t}
-                        </span>
-                    );
-					
-					
+                        {text.map((t, i) => {
 
-                })}
-            </h1>
+                            if (i === 0) {
+                                usedFrames = 0
+                            } else {
+                                nFramesToUse = Math.round((numberOfFrames * 0.80 - usedFrames) / (nWords - i + 1))
+                                usedFrames = usedFrames + nFramesToUse
+                            }
+
+                            return (
+                                <span
+                                    key={t + '-' + i}
+                                    style={{
+                                        color: 'white',
+                                        marginLeft: 20,
+                                        marginRight: 20,
+                                        transform: `scale(${spring({
+                                            fps: videoConfig.fps,
+                                            frame: frame - usedFrames,
+                                            config: {
+                                                damping: 100,
+                                                stiffness: 200,
+                                                mass: 0.5,
+                                            },
+                                        })})`,
+                                        display: 'inline-block',
+                                    }}
+                                >
+                                    {t}
+                                </span>
+                            );
+
+
+
+                        })}
+                    </h1>
+                </div>
+            </div>
         </>
     );
 };
